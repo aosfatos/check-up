b26881c6f6789cfec384a2974cd401241a030377
rhenanbartels <rhenan.bartels@gmail.com>
2024-05-28 09:24:09 -0300

    Remove archiver

--- spiders/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class AdItem(scrapy.Item):
-    title = scrapy.Field()
-    external_link = scrapy.Field()
-    thumbnail = scrapy.Field()
-    tag = scrapy.Field()
-    original_url = scrapy.Field()
