2cfb7759168e95e6501f5ca79e3475623cc1a68a
rhenanbartels <rhenan.bartels@gmail.com>
2024-05-30 19:17:12 -0300

    Fix crawl task

--- crawl.py
+++ crawl.py
@@ -1,15 +1,7 @@
-import sched
-import time
-
 from scrapy.crawler import CrawlerProcess
 
+from plog import logger
 from spiders.base import BaseSpider
-
-
-scheduler = sched.scheduler(time.time, time.sleep)
-
-duration = 86_400  # every 24h
-
 
 process = CrawlerProcess(
     settings={
@@ -18,21 +10,13 @@ process = CrawlerProcess(
 )
 
 
-def event_loop():
-    main()
-    scheduler.enter(duration, 1, event_loop)
-
-
 def run():
-    event_loop()
-    scheduler.run()
-
-
-def main():
     for crawler_class in BaseSpider.__subclasses__():
         process.crawl(crawler_class)
-        process.start()
+    process.start()
 
 
 if __name__ == "__main__":
+    logger.info("Starting crawlers...")
     run()
+    logger.info("Done!")
